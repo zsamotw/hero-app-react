dc4e90f5e64e8c6a090e5e8ad4e1a192f25286aa
zsamotw <donkemot@interia.pl>
2020-07-18 19:49:24 +0200

    Add Confirmation Dialog.

---
+++ src/components/ConfirmationDialog/ConfirmationDialog.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+
+export default function ConfirmationDialog(props) {
+  const [open, setOpen] = React.useState(false)
+  const { isOpen, onOk, onCancel, okButtonContent, cancelButtonContent, content, title } = props
+
+  useEffect(() => {
+    setOpen(isOpen)
+  }, [isOpen])
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">{content}</DialogContentText>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={onCancel} color="primary">
+            {cancelButtonContent}
+          </Button>
+          <Button onClick={onOk} color="primary" autoFocus>
+            {okButtonContent}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
